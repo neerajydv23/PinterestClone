fb4c048c840bb79d5e827755d9ef7b2d16a6b9c9
Neeraj <yneeraj082r@gmail.com>
2024-06-08 11:21:06 +0530

    added validations

--- routes/users.js
+++ routes/users.js
@@ -14,12 +14,48 @@ const Connection = async ()=>{
 Connection();
 
 const userSchema = mongoose.Schema({
-  username: String,
-  name:String,
-  email:String,
-  password:String,
+  username: {
+    type: String,
+    unique: true,
+    required: [true, 'Username is required'],
+    trim: true,
+    validate: {
+      validator: function (value) {
+        // Check if the username contains any whitespace
+        return !/\s/.test(value);
+      },
+      message: 'Username must not contain spaces'
+    }
+  },
+   name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+  },
+  email:{
+    type : String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+  password:{
+    type : String,
+    required: [true, 'Password is required'],
+
+  },
   profileImage:String,
-  contact:Number,
+  contact: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^([0-9]{10}$)/.test(v);
+      }
+    },
+    required: [true, 'Contact is required'],
+    trim: true,
+  },
   boards:{
     type:Array,
     default:[]
